2887e9f9dfb947c14959561ca2437ac0c7fed1aa
zh793060640 <793060640@qq.com>
2017-03-07 15:03:57 +0800

    增加一个baselistfragment

---
+++ speed/app/src/main/java/com/zhanghao/speed/speed/base/BaseListFragment.java
@@ -0,0 +1,34 @@
+package com.zhanghao.speed.speed.base;
+
+/**
+ * Created by PC on 2017/3/7.
+ * 作者 ：张浩
+ * 作用：
+ */
+
+public class BaseListFragment extends BaseFragment {
+    @Override
+    public int getLayoutRes() {
+        return 0;
+    }
+
+    @Override
+    public void initViews() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initToolbar() {
+
+    }
+
+    @Override
+    public void initDagger() {
+
+    }
+}
